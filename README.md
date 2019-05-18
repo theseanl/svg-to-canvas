2aa1bb32d60c18cc4f2ec894402c07c23bbab58a
theseanl <i73hi64d0wr5df8pckig@gmail.com>
2019-05-18 01:22:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# svg-to-canvas
+
+A command line tool for converting static SVGs into Javascript canvas API calls.
+
+Based on [svg2canvas by samsha](https://github.com/samsha/svg2canvas), which is again based on [canvg](https://github.com/canvg/canvg). It uses puppeteer and applies does code formatting to automate and batch process conversion. Note that sometimes a bug of canvg leads to canvas API calls with defects, so users should manually verify that the resulting JS draws what is expected.
+
+## Installation
+
+```
+yarn install
+```
+
+## Usage
+
+```
+node convert.js [glob_of_svg_files] [output_file_name]
+```
+```
+// output.js - one line per file, ./path/to/svg_file.svg is converted to:
+var svg_file = function (ctx) { ctx.save(); /* ... */ ctx.restore(); };
+```
---
+++ convert.js
@@ -0,0 +1,81 @@
+/**
+ * convert.js "glob_to_svgs" "output_file_name"
+ */
+
+const fs = require('async-file');
+const path = require('path');
+const fg = require('fast-glob');
+const puppeteer = require('puppeteer');
+ 
+let globToSvgs = process.argv[2];
+let outputFileName = process.argv[3] || "./output.js";
+
+// Starts a local server
+const express = require('express');
+const port = 3000;
+
+async function launchALocalServer() {
+	const app = express();
+	app.use(express.static(__dirname))
+	
+	return new Promise((resolve, reject) => {
+		app.listen(port, (err) => {
+			if (err) {
+				console.error('server startup failed');
+				console.error(err);
+				reject(err);
+			} else {
+				console.info(`Listening to port ${port}!`);
+				resolve();
+			}
+		});		
+	});
+}
+
+// Loads the conversion page with puppeteer
+async function loadConversionPage() {
+	await launchALocalServer();
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage();
+	await page.goto(`http://localhost:${port}/index.html`);
+	page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+	return { browser, page };
+}
+
+let timer = setTimeout(() => {
+	console.log('Operation taking too long');
+	process.exit(1);
+}, 3 * 60 * 1000);
+
+async function main() {
+	const [paths, _] = await Promise.all([fg(globToSvgs), loadConversionPage()]);
+	const { browser, page } = _;
+	const output = await Promise.all(paths.map(async (svgPath) => {
+		console.log("Converting " + svgPath);
+		const svgStr = await fs.readFile(svgPath, 'utf8');
+		const fileName = path.parse(svgPath).name;
+		
+		try {		
+			const converted = await page.evaluate(
+				`convert_to_canvas_instructions(
+					${JSON.stringify(fileName)},
+					${JSON.stringify(svgStr)}
+				)`
+			);
+			return converted;
+		} catch (e) {
+			console.error(`Error during canvg call for ${fileName}: `, e);
+			return '';
+		}
+	}));
+	console.log(`Writing ${outputFileName}...`);
+	await Promise.all([
+		fs.writeFile(outputFileName, output.join("\n")),
+		browser.close()
+	]);
+	console.log('done');
+	clearTimeout(timer);
+	process.exit(0);
+}
+
+main();
